2a609e9807c9d919b27ab612bed7ffdf94e72371
Panupong Prueksa <panupong.itkmitl@gmail.com>
2018-05-03 17:25:37 +0700

    Improvement on Judge core
    - Can check for correct input/output of code with testcase from database.
    - No progress on timeout/memory restriction.
    - Judge core can clean up used container in docker.
    - Added memory and time limit on container.
    - Secured string compare with "===" and "!=="

--- func/Judge.php
+++ func/Judge.php
@@ -16,13 +16,58 @@ class Judge
 
     public function judging($file)
     {
-        /*$file_name = $this->codeUploader($file);
+        //prepare resource for sandbox to execute on!
+        //upload python code file with named : exercise-{studentid}-{exerciseid}.py
+        $code_file_name = $this->codeUploader($file);
+        $case_data = $this->getCaseInputOutput();
+        $testcase_list = array();
+        $input_counter = 1;
+        foreach ($case_data as $item) {
+            //upload input file with named : testcase-{studentid}-{exerciseid}-{testcasecount}.txt
+            if ($item["input"] !== "-") {
+                array_push($testcase_list, array(
+                    "input" => $this->inputUploader($input_counter, $item["input"]),
+                    "output" => $item["output"]
+                ));
+            } else {
+                array_push($testcase_list, array(
+                    "input" => "-",
+                    "output" => $item["output"]
+                ));
+            }
+            $input_counter += 1;
+        }
+        /*Execute uploaded resource
+          1. Create Container for execute code and bind container volume to host directory {/python-judge}
+          2. Start that container peacefuly
+          3. Create excecute cmd instance for run code on started container
+          4. Run that execute instance goddamn it!
+          5. Kill that container after done
+          6. Remove input file if there're any
+         */
+        $exec_result = array();
         $container_id = $this->createCodeContainer();
         $container_start_result = $this->startContainer($container_id);
-        $exec_id = $this->createExecuter($container_id, $file_name);
-        $exec_result = $this->startExecuter($exec_id);
+        $case_counter = 1;
+        foreach ($testcase_list as $testcase_item) {
+            $exec_id = "AHH";
+            if ($testcase_item["input"] !== "-") {
+                $exec_id = $this->createExecuter($container_id, $code_file_name, true, $testcase_item["input"]);
+            } else {
+                $exec_id = $this->createExecuter($container_id, $code_file_name);
+            }
+            /*Need to use preg_replace() to remove special control char that coming from docker execute api response
+            solution from below
+            https://stackoverflow.com/questions/1497885/remove-control-characters-from-php-string
+            */
+            $result = preg_replace('/[\x00-\x1F\x7F]/', '', $this->startExecuter($exec_id));    
+            $converted_res = ($testcase_item["output"] === $result) ? 'true' : 'false';
+            array_push($exec_result, array("case_number" => $case_counter, "result" => $converted_res));
+            $case_counter += 1;
+        }
+        $this->killContainer($container_id);
 
-        return $exec_result;*/
+        return $exec_result;
     }
 
     private function codeUploader($file_input)
@@ -49,17 +94,29 @@ class Judge
         $file_name = "";
         $con = ftp_connect($ftp_host, $ftp_port, $ftp_timeout);
         ftp_login($con, $ftp_user, $ftp_pass);
-        $file_name = "testcase-" . $this->student_id . "-" . $this->exercise_id . $input_count . ".txt";
+        $file_name = "testcase-" . $this->student_id . "-" . $this->exercise_id . "-" . $input_count . ".txt";
         $file_path = $ftp_path . $file_name;
         $fp = fopen('php://temp', 'r+');
         fwrite($fp, $input);
         rewind($fp);
-        ftp_put($con, $file_path, $fp, FTP_ASCII);
+        ftp_fput($con, $file_path, $fp, FTP_ASCII);
         ftp_close($con);
         return $file_name;
     }
 
-    private function startJudge($file)
+    private function removeInput($input_list)
+    {
+        global $ftp_host, $ftp_port, $ftp_timeout, $ftp_path, $ftp_user, $ftp_pass;
+        $con = ftp_connect($ftp_host, $ftp_port, $ftp_timeout);
+        ftp_login($con, $ftp_user, $ftp_pass);
+        foreach ($input_list as $file_input) {
+            $file_path = $ftp_path . $file_input["file_name"];
+            ftp_delete($con, $file_path);
+        }
+        ftp_close($con);
+    }
+
+    private function getCaseInputOutput()
     {
         global $mysql_server, $username, $password, $database;
         $result = array();
@@ -68,15 +125,13 @@ class Judge
         $stmt->bind_param("s", $this->exercise_id);
         $stmt->execute();
         $stmt->bind_result($case_input, $case_output);
-        $case_counter = 1;
+        $input_output_counter = 1;
         while ($stmt->fetch()) {
-            $file_name = $this->codeUploader($file);
-            $case_file_name = $this->inputUploader($case_counter, $case_input);
-            $container_id = $this->createCodeContainer();
-            $this->startContainer($container_id);
-            $case_counter++;
+            array_push($result, array("input" => $case_input, "output" => $case_output));
+            $input_output_counter += 1;
         }
         $stmt->close();
+        return $result;
     }
 
     private function createCodeContainer()
@@ -87,12 +142,20 @@ class Judge
             "AttachStdout" => true,
             "AttachStderr" => true,
             "OpenStdin" => true,
-            "Tty" => true,
+            "Tty" => false,
             "Volumes" => array("/python-judge" => json_decode("{}")),
             "Image" => "python-sandbox",
             "NetworkDisabled" => true,
             "StopSignal" => "SIGKILL",
-            "HostConfig" => array("Binds" => array("/python-judge:/python-judge"))
+            "HostConfig" => array(
+                "Binds" => array(
+                    "/python-judge:/python-judge"
+                ),
+                "Memory" => (intval($this->memory_limit) * 1048576),
+                "CpuQuota" => (intval($this->time_limit) * 1000000),
+                "OomKillDisable" => false,
+                "AutoRemove" => true,
+            )
         ), JSON_UNESCAPED_SLASHES);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "http://" . $sandbox_ip . "/containers/create");
@@ -124,9 +187,13 @@ class Judge
         return $server_output;
     }
 
-    private function createExecuter($container_id, $file_name)
+    private function createExecuter($container_id, $file_name, $input = false, $file_input_name = "")
     {
         global $sandbox_ip, $sandbox_port;
+        $execute_cmd = "python " . $file_name;
+        if ($input == true) {
+            $execute_cmd .= " < " . $file_input_name;
+        }
         $docker_exec_create = json_encode(array(
             "AttachStdin" => false,
             "AttachStdout" => true,
@@ -134,7 +201,7 @@ class Judge
             "DetachKeys" => "ctrl-p,ctrl-q",
             "Tty" => false,
             "Cmd" =>
-                array("/bin/sh", "-c", "cd python-judge && python " . $file_name)
+                array("/bin/sh", "-c", "cd python-judge && " . $execute_cmd)
         ), JSON_UNESCAPED_SLASHES);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "http://" . $sandbox_ip . "/containers/" . $container_id . "/exec");
@@ -171,6 +238,21 @@ class Judge
         );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec($ch);
+        curl_close($ch);
+        return $server_output;
+    }
+
+    private function killContainer($container_id)
+    {
+        global $sandbox_ip, $sandbox_port;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://" . $sandbox_ip . "/containers/" . $container_id . "/kill");
+        curl_setopt($ch, CURLOPT_PORT, $sandbox_port);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        echo curl_error($ch);
         curl_close($ch);
         return $server_output;
     }
